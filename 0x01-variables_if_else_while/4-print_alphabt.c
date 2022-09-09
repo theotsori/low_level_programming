866311f39008c1e3c1efe0b276901e23b5090e02
theotsori <theodoretsori@gmail.com>
2022-09-09 07:40:43 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - to get alphabet small letter except for q and e
+ *
+ * Return: laways 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
