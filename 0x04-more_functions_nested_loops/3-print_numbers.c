7537e8794375c3a16bbea572f0ffa6e6295c8a26
theotsori <theodoretsori@gmail.com>
2022-09-15 00:06:25 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints numbers from 0 - 9
+ *
+ * Return: 0
+ *
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
