80990982bf483884703b21527e28e9a1cb188fc9
theotsori <theodoretsori@gmail.com>
2022-09-15 13:12:41 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,15 +16,15 @@ int main(void)
 	{
 		if (a % 3 == 0 && a % 5 != 0)
 		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
 		else if (a % 5 == 0 && a % 3 != 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		}
 		else if (a % 3 == 0 && a % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		}
 		else if (a == 1)
 		{
@@ -35,6 +35,6 @@ int main(void)
 			printf(" %d", a);
 		}
 	}
-	printf("/n");
+	printf("\n");
 	return (0);
 }
