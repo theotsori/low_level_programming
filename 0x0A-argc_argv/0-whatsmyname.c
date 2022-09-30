e0ee481fc26bd073bdc8f6adb447993c47095d35
theotsori <theodoretsori@gmail.com>
2022-09-30 10:51:15 -0700

    Changename

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: comandline arguements 
+ * @argv: array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,4 @@
+Project assignment on argc and argv 
+0. It ain't what they call you, it's what you answer to
+1. Silence is arguememt carried out by other means
+2. The best argument against democracy is a five-minute conversation with the average voter.
