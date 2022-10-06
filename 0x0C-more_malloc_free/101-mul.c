bb508ed9ea4e95f8d4925f2f7c44ea4ccbedba0c
theotsori <theodoretsori@gmail.com>
2022-10-06 14:51:08 -0700

    adv malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,28 +4,33 @@
 #include <limits.h>
 /**
  * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
+ * @argc: number of arguments
+ * @argv: arguments
  *
- * Return: int
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	unsigned long mul;
 	int i, j;
-		if (argc != 3)
-		{ printf("Error\n");
-		exit(98); }
-		for (i = 1; i < argc; i++)
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			for (j = 0; argv[i][j] != '\0'; j++)
+			if (argv[i][j] > 57 || argv[i][j] < 48)
 			{
-				if (argv[i][j] > 57 || argv[i][j] < 48)
-				{  printf("Error\n");
-				exit(98); }
+				printf("Error\n");
+				exit(98);
 			}
 		}
-		mul = atoi(argv[1]) * atoi(argv[2]);
-		printf("%lu\n", mul);
+	}
+	mul = atol(argv[1]) *(atol(argv[2]));
+	printf("%lu\n", mul);
 	return (0);
 }
