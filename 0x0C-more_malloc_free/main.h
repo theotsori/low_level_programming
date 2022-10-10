3a6bdb170a075966f201edf52c6dac052cb05eb4
theotsori <theodoretsori@gmail.com>
2022-10-10 00:47:10 -0700

    Last mul update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,10 +1,13 @@
-#ifndef MAIN_H
+#ifndef MAI9N_H
 #define MAIN_H
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
 
 #endif /* main.h */
